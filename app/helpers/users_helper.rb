4999690c7fbbb4794c0a4af35fe1059cc9076c05
Karthik Muthupalaniappan <karthik.m@imaginea.com>
2012-02-28 02:34:24 +0530

    add helper for header display, test for it

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,5 @@
 module UsersHelper
+  def header(user=nil)
+    user ? "#{user.name}'s Message Board" : "The Message Board"
+  end
 end
--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe UsersHelper do
-  
+  it 'should test the header' do
+    helper.header.should == "The Message Board"
+  end
 end
