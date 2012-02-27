a8184016095474c4960db23ae95ead23cba4b000
Karthik Muthupalaniappan <karthik.m@imaginea.com>
2012-02-27 12:02:47 +0530

    initial commit to the repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Viralfeed::Application.config.secret_token = 'f037e1f74daac7cc74700f67248368c45e890287b4f2c8c43088afb411400e2d5ab85da21d77b88307f1084b66b2e26231b6ad319e9ab3e96f35105ddf1f4080'
